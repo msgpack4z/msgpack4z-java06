6e8a6aec5aa9380ee72de7ca93866eb9d5b6b600
xuwei-k <6b656e6a69@gmail.com>
2016-07-29 08:17:52 +0900

    sbt 0.13.12

--- project/Build.scala
+++ project/Build.scala
@@ -1,94 +1,17 @@
-import sbt._, Keys._
-import sbtrelease._
-import xerial.sbt.Sonatype._
-import sbtrelease.ReleasePlugin.autoImport._
-import ReleaseStateTransformations._
-import com.typesafe.sbt.pgp.PgpKeys
+object build {
 
-object build extends Build {
-
-  private def gitHash: String = scala.util.Try(
+  def gitHash: String = scala.util.Try(
     sys.process.Process("git rev-parse HEAD").lines_!.head
   ).getOrElse("master")
 
-  private val msgpack4zJava06Name = "msgpack4z-java06"
+  val msgpack4zJava06Name = "msgpack4z-java06"
 
   val modules = msgpack4zJava06Name :: Nil
 
-  private[this] val unusedWarnings = (
+  val unusedWarnings = (
     "-Ywarn-unused" ::
     "-Ywarn-unused-import" ::
     Nil
-  )
-
-  lazy val msgpack4zJava06 = Project("msgpack4z-java06", file(".")).settings(
-    ReleasePlugin.extraReleaseCommands ++ sonatypeSettings: _*
-  ).settings(
-    resolvers += Opts.resolver.sonatypeReleases,
-    fullResolvers ~= {_.filterNot(_.name == "jcenter")},
-    autoScalaLibrary := false,
-    crossPaths := false,
-    name := msgpack4zJava06Name,
-    javacOptions in compile ++= Seq("-target", "7", "-source", "7"),
-    javacOptions in (Compile, doc) ++= Seq("-locale", "en_US"),
-    commands += Command.command("updateReadme")(UpdateReadme.updateReadmeTask),
-    libraryDependencies ++= (
-      ("org.msgpack" % "msgpack" % "0.6.12") ::
-      ("com.github.xuwei-k" % "msgpack4z-api" % "0.2.0") ::
-      Nil
-    ),
-    releaseProcess := Seq[ReleaseStep](
-      checkSnapshotDependencies,
-      inquireVersions,
-      runClean,
-      runTest,
-      setReleaseVersion,
-      commitReleaseVersion,
-      UpdateReadme.updateReadmeProcess,
-      tagRelease,
-      ReleaseStep(state => Project.extract(state).runTask(PgpKeys.publishSigned, state)._1),
-      setNextVersion,
-      commitNextVersion,
-      UpdateReadme.updateReadmeProcess,
-      pushChanges
-    ),
-    credentials ++= PartialFunction.condOpt(sys.env.get("SONATYPE_USER") -> sys.env.get("SONATYPE_PASS")){
-      case (Some(user), Some(pass)) =>
-        Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
-    }.toList,
-    organization := "com.github.xuwei-k",
-    homepage := Some(url("https://github.com/msgpack4z")),
-    licenses := Seq("MIT License" -> url("http://www.opensource.org/licenses/mit-license.php")),
-    scalacOptions ++= (
-      "-deprecation" ::
-      "-unchecked" ::
-      "-Xlint" ::
-      "-language:existentials" ::
-      "-language:higherKinds" ::
-      "-language:implicitConversions" ::
-      Nil
-    ) ::: unusedWarnings,
-    scalaVersion := "2.11.7",
-    crossScalaVersions := scalaVersion.value :: Nil,
-    pomExtra :=
-      <developers>
-        <developer>
-          <id>xuwei-k</id>
-          <name>Kenji Yoshida</name>
-          <url>https://github.com/xuwei-k</url>
-        </developer>
-      </developers>
-      <scm>
-        <url>git@github.com:msgpack4z/msgpack4z-java06.git</url>
-        <connection>scm:git:git@github.com:msgpack4z/msgpack4z-java06.git</connection>
-        <tag>{if(isSnapshot.value) gitHash else { "v" + version.value }}</tag>
-      </scm>
-    ,
-    description := "msgpack4z"
-  ).settings(
-    Seq(Compile, Test).flatMap(c =>
-      scalacOptions in (c, console) ~= {_.filterNot(unusedWarnings.toSet)}
-    )
   )
 
 }
